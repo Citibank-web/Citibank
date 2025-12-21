8e6b668e218930c8c341cd92d16e22b183ad1008
Citibank-web <benzintegrateict@gmail.com>
2025-12-21 03:47:20 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-// js/firebase.js
+// Import Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
@@ -8,11 +8,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyAhH_bvUtQiOxvENXgh2Cin0fmMWsxi0z8",
   authDomain: "lite-584ca.firebaseapp.com",
   projectId: "lite-584ca",
-  storageBucket: "lite-584ca.firebasestorage.app",
+  storageBucket: "lite-584ca.appspot.com", // fixed
   messagingSenderId: "861872738616",
   appId: "1:861872738616:web:8b086a791e3745e5f171cd"
 };
 
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
