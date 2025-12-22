805c6a82e1e850e6586f05ecd028ebdb4063109f
Citibank-web <benzintegrateict@gmail.com>
2025-12-22 06:36:39 +0100

    Create Supabase.js

---
+++ Supabase.js
@@ -0,0 +1,8 @@
+<script type="module">
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://aaljhxyrqnewvhejtoik.supabase.co",
+  "sb_publishable_52hZQtxrDZB4k_fLi8S0GQ_j5wma9lz"
+);
+</script>
